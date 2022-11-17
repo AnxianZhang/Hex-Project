aa38446432870fca27671d231537e9c3794831d3
linxingt <xingtonglin011016@gmail.com>
2022-11-17 16:11:34 +0100

    test

--- src/test/TestPlateau.java
+++ src/test/TestPlateau.java
@@ -11,7 +11,7 @@ import player.Identity;
 import player.Player;
 
 class TestPlateau {
-    // test de tout
+    // test de toutes les classes
     @Test
     void test() {
         final int taille = 4;
