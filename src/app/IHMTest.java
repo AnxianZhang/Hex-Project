232642f9ed5c178c4a66baf408b92addc0fa6201
redou <redouaneo.ro@gmail.com>
2022-12-03 14:42:48 +0100

    ajout fichier ihmTest

---
+++ src/app/IHMTest.java
@@ -0,0 +1,12 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IHMTest {
+
+    @Test
+    void recuperer_type_de_joueur() {
+    }
+}
