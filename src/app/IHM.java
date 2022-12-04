be8e551576c7d4bedd3b0d3f831fa9e17c109bae
redou <redouaneo.ro@gmail.com>
2022-12-04 16:58:46 +0100

    Ajout de la feature choix d'une case

--- src/app/IHM.java
+++ src/app/IHM.java
@@ -1,14 +1,16 @@
 package app;
 
 
+import game.Plateau;
 import game.Stat;
 import player.Identity;
+import player.Player;
 
 
 import java.util.Scanner;
 public class IHM {
 
-    private final static int NB_TYPE_JOUEUR = Identity.values().length;
+
 
     //HashMap<Integer, String> Choix_type_joueur = new HashMap<>();
 
@@ -17,25 +19,50 @@ public class IHM {
 
     }
     public Identity recuperer_type_de_joueur(){
+        final int  PREMIER_ELEMENT = 0;
+
         System.out.println("Choissisez le type du joueur par un numéro" );
         afficher_choix_possible();
 
+        int choix = demander_un_integer( PREMIER_ELEMENT, Identity.values().length);
 
+        return Identity.values()[choix];
+    }
+
+
+    private void afficher_choix_possible(){
+
+        for (int i = 0; i < Identity.values().length ;++i){
+            System.out.println("choix " + i + " " + Stat.values()[i].toString());
+        }
+    }
+    public void afficher_resultat(Player gagnant , Player perdant){
+        System.out.println("Les " + gagnant.getPawnColor().name() + " ont gagner ");
+        System.out.println("Les " + perdant.getPawnColor().name() + " ont perdu");
+    }
+
+    public int demander_coup_a_jouer(Plateau p){
+        final int  PREMIERE_CASE_DU_PLATEAU = 0;
+        System.out.println("Choississez une case valide a jouer sur le plateau");
+        return demander_un_integer(PREMIERE_CASE_DU_PLATEAU,p.taille());
+    }
+
+
+    private int demander_un_integer(int borne_inferieur_incluse, int borne_superieur){
         while (true) {
 
             if(!IsInt()){}
 
             else  {
                 int choix = sc.nextInt() ;
-                if (choix <NB_TYPE_JOUEUR && choix >0 ){
-                    return Identity.values()[choix];
+                if (choix < borne_superieur && choix >=borne_inferieur_incluse ){
+                    return choix;
                 }
                 System.out.print("Un nombre valide est requis, saisissez votre choix : ");
                 sc.nextLine();
 
             }
         }
-
     }
 
     private boolean IsInt(){
@@ -46,17 +73,7 @@ public class IHM {
             return false;
 
         }
-        System.out.println("fdsfjhcaca");
-        return true;
-    }
-    private void afficher_choix_possible(){
 
-        for (int i = 0; i < NB_TYPE_JOUEUR ;++i){
-            System.out.println("choix " + i + " " + Stat.values()[i].toString());
-        }
-    }
-    public void afficher_resultat(Stat gagnant , Stat perdant){
-        System.out.println("Les " + gagnant.name() + " ont gagner ");
-        System.out.println("Les " + perdant.name() + " ont perdu");
+        return true;
     }
 }
