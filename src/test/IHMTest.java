14829772d4e8492a779e1b7d1db318e805ee4241
redou <redouaneo.ro@gmail.com>
2022-12-10 21:01:41 +0100

    commit reverse

--- src/test/IHMTest.java
+++ src/test/IHMTest.java
@@ -3,11 +3,12 @@ package test;
 import app.IIHM;
 import IHM.IHM;
 import org.junit.jupiter.api.Test;
+import player.Human;
 import player.Identity;
 import player.Player;
 
 class IHMTest {
-    /*private IIHM screen = new IHM();
+    private IIHM screen = new IHM();
     @Test
     void recuperer_type_de_joueur() {
 
@@ -16,6 +17,6 @@ class IHMTest {
 
     @Test
     void afficher_resultat() {
-        screen.afficher_resultat(new Player(Identity.HUMAN), new Player(Identity.HUMAN));
-    }*/
+        screen.afficher_resultat(Identity.HUMAN.name(), Identity.HUMAN.name());
+    }
 }
