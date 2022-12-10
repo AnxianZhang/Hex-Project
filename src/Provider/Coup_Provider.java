e887f5c9350b99a7310df73a0e29020437c408bf
redou <redouaneo.ro@gmail.com>
2022-12-10 20:27:22 +0100

    -ajout de cette classe provider qui va choisir la bonne facon de choisir un coup pour les joueurs

--- src/Provider/Coup_Provider.java
+++ src/Provider/Coup_Provider.java
@@ -1,4 +1,29 @@
 package Provider;
 
+import IHM.IHM;
+import game.Plateau;
+import game.Stat;
+
+import java.util.Random;
+
 public class Coup_Provider {
+    protected IHM console_joueur = new IHM();
+    public int provide_coup_AI(Plateau plateau) {
+
+        Random r = new Random();
+        int randCase = r.nextInt(plateau.getNbOfUsableCase()) + 1;
+        int ctp = 0;
+        for (int i = 0; i < plateau.taille(); ++i)
+            for (int j = 0; j < plateau.taille(); ++j) {
+                if (plateau.isEmpty(i, j)) ++ctp;
+                if (ctp == randCase)
+                    return (i * plateau.taille() + j);
+            }
+        return -1;
+
+    }
+    public int provide_coup_HUMAN(Stat couleur , Plateau p){
+        return console_joueur.demander_coup_a_jouer(p,couleur);
+    }
+
 }
