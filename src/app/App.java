7342ae1f79e3a7acae700d7a8369f0ee2da68f57
redou <redouaneo.ro@gmail.com>
2022-12-10 20:30:12 +0100

    modification légères de la classe pour s'adapter aux nouvelles classes et types

--- src/app/App.java
+++ src/app/App.java
@@ -3,6 +3,8 @@ package app;
 import IHM.IHM;
 import game.Plateau;
 import game.Stat;
+import player.Human;
+import player.IA;
 import player.Identity;
 import player.Player;
 
@@ -10,26 +12,32 @@ import java.util.ArrayList;
 
 public class App {
     static final int TAILLE_JEU = 4;
+
     static Plateau plateau = new Plateau(TAILLE_JEU);
     static ArrayList<Player> joueurs = new ArrayList<>();
     static Player joueur_courant;
     static private int compteur_joueur = 0;
     public static void main(String[] args) {
+        int choix_du_joueur_courant;
         IIHM ihm = new IHM();
 
-        Player p1 = new Player(ihm.recuperer_type_de_joueur()); // imparaire = WHITE
-        Player p2 = new Player(ihm.recuperer_type_de_joueur()); // pair = BLACK
+        Player p1 = ihm.recuperer_type_de_joueur() == Identity.HUMAN? new Human(Identity.HUMAN): new IA(Identity.IA); // imparaire = WHITE
+        Player p2 = ihm.recuperer_type_de_joueur() == Identity.HUMAN? new Human(Identity.HUMAN): new IA(Identity.IA);; // pair = BLACK
+        joueurs.add(p1);
+        joueurs.add(p2);
         ihm.mettre_a_jour_plateau(plateau);
+
         while (!plateau.isFull()) {
             joueur_courant = get_joueur();
-            plateau.play(joueur_courant.getChoice(), joueur_courant.getPawnColor());
-            ihm.afficher_le_coup(joueur_courant,joueur_courant.getChoice());
+            choix_du_joueur_courant = joueur_courant.getChoice(plateau);
+            plateau.play(choix_du_joueur_courant, joueur_courant.getPawnColor());
             ihm.mettre_a_jour_plateau(plateau);
+            ihm.afficher_le_coup(joueur_courant,choix_du_joueur_courant);
             if (plateau.isWin() == Stat.WHITE) {
                 ihm.afficher_resultat(p1, p2);
                 return;
             }
-            if (plateau.isWin() == Stat.BLACK) {
+            else if (plateau.isWin() == Stat.BLACK) {
                 ihm.afficher_resultat(p2, p1);
                 return;
             }
@@ -39,8 +47,9 @@ public class App {
         //System.out.println(ihm.recuperer_type_de_joueur());
     }
     static private Player get_joueur(){
+
         ++compteur_joueur;
-        compteur_joueur = compteur_joueur%2;
+        compteur_joueur = compteur_joueur % joueurs.size();
         return joueurs.get(compteur_joueur);
     }
 }
