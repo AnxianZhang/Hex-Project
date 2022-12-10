d5529532e0cba1985c891e6d3652ad3398032747
redou <redouaneo.ro@gmail.com>
2022-12-10 13:26:32 +0100

    - refactoring de la fonction de jeu du main

--- src/app/App.java
+++ src/app/App.java
@@ -6,72 +6,41 @@ import game.Stat;
 import player.Identity;
 import player.Player;
 
+import java.util.ArrayList;
+
 public class App {
+    static final int TAILLE_JEU = 4;
+    static Plateau plateau = new Plateau(TAILLE_JEU);
+    static ArrayList<Player> joueurs = new ArrayList<>();
+    static Player joueur_courant;
+    static private int compteur_joueur = 0;
     public static void main(String[] args) {
-        IIHM ihm=new IHM();
-        final int TAILLE_JEU = 4;
+        IIHM ihm = new IHM();
+
         Player p1 = new Player(ihm.recuperer_type_de_joueur()); // imparaire = WHITE
         Player p2 = new Player(ihm.recuperer_type_de_joueur()); // pair = BLACK
-        Game g= new Game(p1, p2 ,TAILLE_JEU);
-        ihm.mettre_a_jour_plateau(g);
-        while (true){
-            if(p1.getIsIA() && p2.getIsIA()){
-                g.setPlayersChoice();
-                ihm.mettre_a_jour_plateau(g);
-                if(g.isWin(p1.getPawnColor())) {
-                    ihm.afficher_resultat(p1,p2);
-                    return;
-                }
-                if(g.isWin(p2.getPawnColor())){
-                    ihm.afficher_resultat(p2,p1);
-                    return;
-                }
+        ihm.mettre_a_jour_plateau(plateau);
+        while (!plateau.isFull()) {
+            joueur_courant = get_joueur();
+            plateau.play(joueur_courant.getChoice(), joueur_courant.getPawnColor());
+            ihm.afficher_le_coup(joueur_courant,joueur_courant.getChoice());
+            ihm.mettre_a_jour_plateau(plateau);
+            if (plateau.isWin() == Stat.WHITE) {
+                ihm.afficher_resultat(p1, p2);
+                return;
             }
-            else if((p1.getIsIA() && !p2.getIsIA())){
-                g.setPlayersChoice();
-                ihm.mettre_a_jour_plateau(g);
-                if(g.isWin(p1.getPawnColor())) {
-                    ihm.afficher_resultat(p1,p2);
-                    return;
-                }
-                g.setPlayersChoice(ihm.demander_coup_a_jouer(g, p2));
-                ihm.mettre_a_jour_plateau(g);
-                if(g.isWin(p2.getPawnColor())){
-                    ihm.afficher_resultat(p2,p1);
-                    return;
-                }
+            if (plateau.isWin() == Stat.BLACK) {
+                ihm.afficher_resultat(p2, p1);
+                return;
             }
-            else if((!p1.getIsIA() && p2.getIsIA())){
-                g.setPlayersChoice(ihm.demander_coup_a_jouer(g, p1));
-                ihm.mettre_a_jour_plateau(g);
-                if(g.isWin(p1.getPawnColor())) {
-                    ihm.afficher_resultat(p1,p2);
-                    return;
-                }
-                g.setPlayersChoice();
-                ihm.mettre_a_jour_plateau(g);
-                if(g.isWin(p2.getPawnColor())){
-                    ihm.afficher_resultat(p2,p1);
-                    return;
-                }
-            }
-            else {
-                g.setPlayersChoice(ihm.demander_coup_a_jouer(g, p1));
-                ihm.mettre_a_jour_plateau(g);
-                if(g.isWin(p1.getPawnColor())) {
-                    ihm.afficher_resultat(p1,p2);
-                    return;
-                }
-                g.setPlayersChoice(ihm.demander_coup_a_jouer(g, p2));
-                ihm.mettre_a_jour_plateau(g);
-                if(g.isWin(p2.getPawnColor())){
-                    ihm.afficher_resultat(p2,p1);
-                    return;
-                }
-            }
-
         }
 
+
         //System.out.println(ihm.recuperer_type_de_joueur());
+    }
+    static private Player get_joueur(){
+        ++compteur_joueur;
+        compteur_joueur = compteur_joueur%2;
+        return joueurs.get(compteur_joueur);
     }
 }
