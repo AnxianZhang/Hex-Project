86385cc84968b24951563f12a455cbff2b4f4883
Nathan <nathan.collombet@etu.univ-smb.fr>
2022-12-13 03:53:35 +0100

    toute la doc des classes IHM et Player, FINI V1 DOCUMENTATION

--- src/IHM/IHM.java
+++ src/IHM/IHM.java
@@ -12,6 +12,17 @@ public class IHM implements IIHM {
     public IHM(){
 
     }
+
+    /**
+     * Cette méthode permet de proposer à l'utilisateur de choisir le type
+     * de joueur qui va ouer (IA ou humain) et ensuite récupérer cette valeur
+     *
+     * @return retourne un objet humain ou IA de type Identity, ce sera le type du joueur
+     *
+     * @see #afficher_choix_possible()
+     * @see #demander_un_integer(int, int)
+     * @see Identity#values()
+     */
     @Override
     public Identity recuperer_type_de_joueur(){
         final int  PREMIER_ELEMENT = 0;
@@ -24,12 +35,31 @@ public class IHM implements IIHM {
         return Identity.values()[choix];
     }
 
+    /**
+     * Cette méthode permet simplement d'afficher à l'utilisateur les choix
+     * de type de joueur qui peut choisir. C'est une méthode d'affichage.
+     *
+     * @see Identity#values()
+     * @see Identity#toString()
+     * @see Stat#values()
+     */
     private void afficher_choix_possible(){
 
         for (int i = 0; i < Identity.values().length ;++i){
             System.out.println("choix " + i + " " + Stat.values()[i].toString());
         }
     }
+
+    /**
+     * Cette méthode est une méthode d'affichage. Elle permet d'afficher
+     * à l'utilisateur les résultats de la partie.
+     *
+     * @param gagnant paramètre qui fait référence à un joueur
+     * @param perdant paramètre qui fait référence à un joueur
+     *
+     * @see Player#getPawnColor()
+     * @see Stat#name()
+     */
     @Override
     public void afficher_resultat(Player gagnant, Player perdant){
         System.out.println("Les " + gagnant.getPawnColor().name() + " ont gagner ");
@@ -37,15 +67,44 @@ public class IHM implements IIHM {
         System.out.println("La partie est finie");
     }
 
+    /**
+     * Méthode qui permet d'afficher le plateau
+     *
+     * @param p objet plateau
+     */
     @Override
     public void mettre_a_jour_plateau(Plateau p){
         System.out.println(p);
     }
+
+    /**
+     * Cette méthode permet d'afficher le coup d'un joueur, où est-ce qu'il a joué ?
+     *
+     * @param joueur le joueur qui a joué
+     * @param choix la case où le joueur à joué
+     *
+     * @see Stat#name()
+     * @see Player#getPawnColor()
+     */
     @Override
     public void afficher_le_coup(Player joueur, int choix){
         System.out.println( joueur.getPawnColor().name() + "ont jouées à la case " + choix);
     }
 
+    /**
+     * Cette méthode demande aux joueurs où est-ce qu'ils veulent poser leur pion
+     *
+     * @param p le plateau sur lequelle les joueurs jouent
+     * @param joueur le joueur à qui on demande où il va jouer
+     *
+     * @return retourne un entier entre 0 et la taille du plteau, c'est là case
+     * où le joueur va jouer
+     *
+     * @see Stat#name()
+     * @see Player#getPawnColor()
+     * @see #demander_un_integer(int, int)
+     * @see Plateau#taille()
+     */
     @Override
     public int demander_coup_a_jouer(Plateau p, Player joueur){
         final int  PREMIERE_CASE_DU_PLATEAU = 0;
@@ -53,6 +112,18 @@ public class IHM implements IIHM {
         return demander_un_integer(PREMIERE_CASE_DU_PLATEAU,p.taille());
     }
 
+    /**
+     * Cette méthode va récupérer la valeur de la position où le joueur veut jouer
+     *
+     * @param borne_inferieur_incluse début du plateau
+     * @param borne_superieur fin du plateau
+     *
+     * @return retourne un entier qui correspond au choix du joueur
+     *
+     * @see #IsInt() méthode qui va vérifier que c'est un entier
+     * @see Scanner#nextInt()
+     * @see Scanner#nextLine()
+     */
     private int demander_un_integer(int borne_inferieur_incluse, int borne_superieur){
         while (true) {
 
@@ -69,6 +140,14 @@ public class IHM implements IIHM {
         }
     }
 
+    /**
+     * Cette méthode nous indique si la valeur est bien un entier
+     *
+     * @return retourne un booléen : true si la valeur est un entier sinon false
+     *
+     * @see Scanner#hasNextInt()
+     * @see Scanner#nextLine()
+     */
     private boolean IsInt(){
         if (!sc.hasNextInt()) {
 
--- src/player/Player.java
+++ src/player/Player.java
@@ -9,8 +9,13 @@ public class Player{
     private final Stat pawnColor;
 
     /**
+     * Ce constructeur permet d'initialiser tous les paramètres de la classe
+     * en fonction du type du joueur
+     * isIA est initialisé à true si la condition : le type du paramètre est égal à IA
+     * est vraie.
+     * isFirstColor permet de savoir qui va commencer
      *
-     * @param type
+     * @param type objet human ou ia de l'enum Identity
      */
     public Player(Identity type) {
         this.isIA = type == Identity.IA;
@@ -25,18 +30,38 @@ public class Player{
         }
     }
 
+    /**
+     * Méthode qui indique la couleur du pion
+     *
+     * @return retourne un objet de la classe Stat (WHITE,BLACK,VIDE)
+     */
     public Stat getPawnColor(){
         return this.pawnColor;
     }
 
+    /**
+     * Méthode qui indique le choix du joueur pour la position de son pion
+     *
+     * @return retourne un entier en référence à la position sur le plateau
+     */
     public int getChoice() {
         return this.choice;
     }
 
+    /**
+     * Méthode qui permet de faire le choix de la case et de le mettre dans le paramètre choix
+     *
+     * @param inChoice position dans le plateau
+     */
     public void setChoice(int inChoice){
         this.choice = inChoice;
     }
 
+    /**
+     * Méthode qui indique si le joueur est une IA ou non
+     *
+     * @return retourne un booléen : true si c'est une IA sinon false
+     */
     public boolean getIsIA(){
         return this.isIA;
     }
